88e368edcf2644bb0406f025852d8f1d3cf6723c
rckola1011 <rckola1011@gmail.com>
2018-07-04 11:23:32 -0700

    Initial checkin

---
+++ RyanKenward.Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RyanKenward.Strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
